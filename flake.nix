b5df912693663d66d545a3676d265ae8644bd622
Johannes May <johannes@may-se.de>
2024-07-27 19:53:11 +0200

    Add Nix build and module

--- flake.nix
+++ flake.nix
@@ -1,8 +1,7 @@
 # This file is pretty general, and you can adapt it in your project replacing
 # only `name` and `description` below.
-
 {
-  description = "My awesome Rust project";
+  description = "Tibber local HTTP API to Prometheus bridge";
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
@@ -12,19 +11,128 @@
     };
   };
 
-  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
-    flake-utils.lib.eachDefaultSystem 
-      (system:
-        let
-          overlays = [ rust-overlay.overlays.default ];
-          pkgs = import nixpkgs { inherit system overlays; };
-          rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        in
-        {
-          devShell = pkgs.mkShell {
-            packages = [ rust pkgs.cargo pkgs.rustfmt pkgs.rust-analyzer pkgs.pkg-config pkgs.openssl ];
-          };
-        }
-      );
-}
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        overlays = [rust-overlay.overlays.default];
+        pkgs = import nixpkgs {inherit system overlays;};
+        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        build_deps = [pkgs.openssl];
+        build_tools = [pkgs.pkg-config];
+      in {
+        formatter = pkgs.alejandra;
 
+        devShells.default = pkgs.mkShell {
+          packages = [rust pkgs.cargo pkgs.rustfmt pkgs.rust-analyzer] ++ build_deps ++ build_tools;
+        };
+
+        packages = rec {
+          tibber-prometheus-bridge = pkgs.rustPlatform.buildRustPackage {
+            name = "tibber-prometheus-bridge";
+            src = ./.;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+            buildInputs = build_deps;
+            nativeBuildInputs = build_tools;
+          };
+
+          default = tibber-prometheus-bridge;
+        };
+
+        apps = rec {
+          tibber-prometheus-bridge = flake-utils.lib.mkApp {drv = self.packages.${system}.tibber-prometheus-bridge;};
+          default = tibber-prometheus-bridge;
+        };
+      }
+    )
+    // {
+      nixosModules = rec {
+        tibber-prometheus-bridge = {
+          lib,
+          config,
+          system,
+          ...
+        }:
+          with lib; let
+            cfg = config.services.tibber-prometheus-bridge;
+          in {
+            options.services.tibber-prometheus-bridge = {
+              enable = mkEnableOption "the Tibber Prometheus Bridge";
+              bind-address = mkOption {
+                type = types.str;
+                default = "127.0.0.1";
+                description = "The address to listen on for incoming Prometheus requests.";
+              };
+              bind-port = mkOption {
+                type = types.port;
+                default = 8018;
+                description = "The port number to listen on for incoming Prometheus requests.";
+              };
+              tibber-host = mkOption {
+                type = types.str;
+                default = "tibber-host";
+                description = "The upstream tibber gateway host name. You must enable the local HTTP interface in the gateway.";
+              };
+              tibber-admin-password-file = mkOption {
+                type = types.str;
+                description = "A path to a file containing the password for your local tibber host.";
+              };
+
+              enable-prometheus-scrape = mkEnableOption "local Prometheus scrape configuration for Tibber.";
+              prometheus-scrape-job-name = mkOption {
+                type = types.str;
+                default = "tibber";
+                description = "The job name for the local Prometheus scrape job.";
+              };
+
+              user = mkOption {
+                type = types.str;
+                default = "tibber-bridge";
+              };
+            };
+
+            config =
+              mkIf cfg.enable {
+                users.users.${cfg.user} = {
+                  isNormalUser = true;
+                  group = cfg.user;
+                };
+                users.groups.${cfg.user} = {};
+
+                systemd.services.tibber-prometheus-bridge = {
+                  enable = true;
+                  description = "Tibber Prometheus Bridge";
+                  wantedBy = ["multi-user.target"];
+                  serviceConfig = {
+                    Type = "simple";
+                    User = cfg.user;
+                    ExecStart = "${self.packages.${system}.tibber-prometheus-bridge}/bin/tibber-prometheus-bridge -t ${cfg.tibber-host} -b ${cfg.bind-address}:${toString cfg.bind-port} -p ${cfg.tibber-admin-password-file}";
+                  };
+                };
+              }
+              // mkIf cfg.enable-prometheus-scrape {
+                services.prometheus.scrapeConfigs = [
+                  {
+                    job_name = "beira";
+                    static_configs = [
+                      {
+                        targets = ["127.0.0.1:${toString cfg.bind-port}"];
+                      }
+                    ];
+                  }
+                ];
+              };
+          };
+
+        default = tibber-prometheus-bridge;
+      };
+    };
+}
